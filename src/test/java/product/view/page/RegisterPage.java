1fe7fe36a2da02250483d807852ff0c43ddeeb36
koujienami <koji@fortiservices.com>
2018-12-29 11:06:03 +0900

    登録画面のテストケースを追加。

--- src/test/java/product/view/page/RegisterPage.java
+++ src/test/java/product/view/page/RegisterPage.java
@@ -15,12 +15,57 @@ public class RegisterPage {
 	@FindBy(id = "backButton")
 	private SelenideElement backButton;
 
+	@FindBy(id = "dateId")
+	private SelenideElement dateId;
+
+	@FindBy(id = "dateName")
+	private SelenideElement dateName;
+
+	@FindBy(id = "adjustmentYear")
+	private SelenideElement adjustmentYear;
+
+	@FindBy(id = "adjustmentMonth")
+	private SelenideElement adjustmentMonth;
+
+	@FindBy(id = "adjustmentDay")
+	private SelenideElement adjustmentDay;
+
 	public String title() {
 		return Selenide.title();
 	}
 
 	public SimulationPage シミュレーションページへ戻る() {
 		backButton.click();
+		return page(SimulationPage.class);
+	}
+
+	public RegisterPage 日付IDは(String 日付ID) {
+		dateId.setValue(日付ID);
+		return page(RegisterPage.class);
+	}
+
+	public RegisterPage 日付名は(String 日付名) {
+		dateName.setValue(日付名);
+		return page(RegisterPage.class);
+	}
+
+	public RegisterPage 加減年は(String 加減年) {
+		adjustmentYear.setValue(加減年);
+		return page(RegisterPage.class);
+	}
+
+	public RegisterPage 加減月は(String 加減月) {
+		adjustmentMonth.setValue(加減月);
+		return page(RegisterPage.class);
+	}
+
+	public RegisterPage 加減日は(String 加減日) {
+		adjustmentDay.setValue(加減日);
+		return page(RegisterPage.class);
+	}
+
+	public SimulationPage で新規登録する() {
+		registerButton.click();
 		return page(SimulationPage.class);
 	}
 }
