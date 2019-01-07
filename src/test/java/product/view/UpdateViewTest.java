be091befec3a16a1cc2548da7d2f9b91b93bdc3c
koujienami <test@example.com>
2019-01-07 14:20:51 +0900

    更新画面のテストを追加。

--- src/test/java/product/view/UpdateViewTest.java
+++ src/test/java/product/view/UpdateViewTest.java
@@ -1,5 +1,6 @@
 package product.view;
 
+import static com.codeborne.selenide.Condition.visible;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
@@ -41,5 +42,14 @@ public class UpdateViewTest {
 		SimulationPage actual = page.シミュレーションページへ戻る();
 
 		assertThat(actual.title()).isEqualTo("計算結果確認");
+	}
+
+	@Test
+	public void No2_更新画面で更新できる事() throws Exception {
+		SimulationPage simulationPage = page.日付名は("前年").加減年は("-1").加減月は("0").加減日は("0").で更新する();
+		SimulationPage actual = simulationPage.計算基準日は("20181201").でシミュレーションする();
+
+		actual.検索結果().shouldBe(visible);
+		assertThat(actual.検索結果の件数()).isEqualTo(2);
 	}
 }
--- src/test/java/product/view/page/UpdatePage.java
+++ src/test/java/product/view/page/UpdatePage.java
@@ -15,12 +15,49 @@ public class UpdatePage {
 	@FindBy(id = "backButton")
 	private SelenideElement backButton;
 
+	@FindBy(id = "dateName")
+	private SelenideElement dateName;
+
+	@FindBy(id = "adjustmentYear")
+	private SelenideElement adjustmentYear;
+
+	@FindBy(id = "adjustmentMonth")
+	private SelenideElement adjustmentMonth;
+
+	@FindBy(id = "adjustmentDay")
+	private SelenideElement adjustmentDay;
+
 	public String title() {
 		return Selenide.title();
 	}
 
 	public SimulationPage シミュレーションページへ戻る() {
 		backButton.click();
+		return page(SimulationPage.class);
+	}
+
+	public UpdatePage 日付名は(String 日付名) {
+		dateName.setValue(日付名);
+		return page(UpdatePage.class);
+	}
+
+	public UpdatePage 加減年は(String 加減年) {
+		adjustmentYear.setValue(加減年);
+		return page(UpdatePage.class);
+	}
+
+	public UpdatePage 加減月は(String 加減月) {
+		adjustmentMonth.setValue(加減月);
+		return page(UpdatePage.class);
+	}
+
+	public UpdatePage 加減日は(String 加減日) {
+		adjustmentDay.setValue(加減日);
+		return page(UpdatePage.class);
+	}
+
+	public SimulationPage で更新する() {
+		updateButton.click();
 		return page(SimulationPage.class);
 	}
 }
